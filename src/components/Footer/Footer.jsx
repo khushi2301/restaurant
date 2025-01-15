3c9d88c61ba463ab1ace2f69ffa3e23f3d9294af
khushi2301 <garkhushi12@gmail.com>
2025-01-16 01:06:56 +0530

    msg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,9 @@ const Footer = () => {
           <img src={assets.logo} alt="" />
           <p>Welcome to Tomato, where passion for great food meets an inviting atmosphere. At Tomato, we believe that every meal is an opportunity to create unforgettable moments. Whether you're craving comforting classics or adventurous flavors, our diverse menu offers something for everyone.</p>
           <div className="footer-social-icon">
-          <img src={assets.facebook_icon} alt="" />
+          <a href="https://m.facebook.com/khushi.gar.9/" target="_blank" rel="noopener noreferrer">
+            <img src={assets.facebook_icon} alt="Facebook" />
+          </a>
           <img src={assets.twitter_icon} alt="" />
           <img src={assets.linkedin_icon} alt="" />
           </div>
